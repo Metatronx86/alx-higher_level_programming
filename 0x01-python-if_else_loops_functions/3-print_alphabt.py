0fd60bc230f4093d17638a903b2bfcc12b6cb9d4
Metatronx64 <osagieevba@gmail.com>
2023-05-10 21:36:54 -0700

    except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
 for i in range(97, 123):
-    if i == 113 or i == 101:
+    if chr(i) == 'q' or chr(i) == 'e':
         continue
-    print("{:c}".format(i), end="")
+    print("{}".format(chr(i)), end="")
