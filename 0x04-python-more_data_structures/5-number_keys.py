86f147c9e6e5f0a49e7999c806efdef5d5068d46
Metatronx64 <osagieevba@gmail.com>
2023-05-17 14:52:22 -0700

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Return the number of keys in a dictionary.
+
+    Args:
+        a_dictionary: The dictionary to count the keys from.
+
+    Returns:
+        The number of keys in the dictionary.
+    """
+    return (len(a_dictionary))
+
