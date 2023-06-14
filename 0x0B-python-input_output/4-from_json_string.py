453d49dc6a8719a36704d75577acf9852aae18d3
Metatronx64 <osagieevba@gmail.com>
2023-06-14 15:05:12 -0700

    Json to string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-'''
-Function to convert a JSON string to a Python object (data structure).
-'''
+"""
+Module for from_json_string method.
+"""
+
 
 import json
 
-def from_json_string(my_str):
-'''
-Converts a JSON string to a Python object.
-Args:
-    my_str (str): The JSON string to be deserialized.
 
-Returns:
-    any: The Python object represented by the JSON string.
-'''
-return (json.loads(my_str))
+def from_json_string(my_str):
+    """
+    returns an object represented by a JSON string.
+    """
+    return (json.loads(my_str))
