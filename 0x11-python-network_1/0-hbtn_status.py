cbf65d9e3f58b490961a322c6f410459cf9e7611
Metatronx64 <osagieevba@gmail.com>
2023-09-01 16:06:18 -0700

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""A script that fetches https://intranet.hbtn.io/status and displays information about the response."""
+"""A script that
+fetches https://intranet.hbtn.io/status.
+"""
 
-import urllib.request
-
-def fetch_and_display_status(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read().decode('utf-8')
-
-            print("Body response:")
-            print("\t- type:", type(content))  # Display the type of content (str)
-            print("\t- content:", content)      # Display the content (str)
-
-    except urllib.error.URLError as e:
-        print(f"Error: {e.reason}")
 
 if __name__ == '__main__':
-    url_to_fetch = 'https://intranet.hbtn.io/status'
-    fetch_and_display_status(url_to_fetch)
+    import urllib.request
 
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
