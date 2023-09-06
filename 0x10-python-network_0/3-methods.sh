30b26d25ae0ee1b519750b33cc79f213c1895ae7
Metatronx64 <osagieevba@gmail.com>
2023-09-06 08:01:34 -0700

    All allowed methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL as input and displays all the HTTP methods that the server will accept.
-curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
-
+# displays all HTTP methods the server accepts.
+curl -Is "$1" | grep "Allow:" | cut -d " " -f 2-
