ac9c4b71952769003c2671b24d6b1eb2f8dca8eb
Metatronx64 <osagieevba@gmail.com>
2023-09-07 06:06:43 -0700

    return max integer

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,11 @@
 #!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers using a binary search-like approach.
+    """Finds a peak in list_of_integers"""
 
-    Args:
-        list_of_integers (list): List of unsorted integers.
-
-    Returns:
-        int: The peak element found in the list.
-
-    Note:
-        There may be more than one peak in the list.
-    """
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        # Compare the middle element with its neighbors
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid  # The peak is in the left half (inclusive)
-        else:
-            left = mid + 1  # The peak is in the right half
-
-    return list_of_integers[left]
-
-
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    else:
+        return max(list_of_integers)
